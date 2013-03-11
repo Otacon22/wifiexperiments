dbfdaba6acf4fcf0f84462df61f7dc6626f946cc
Otacon22 <danieleiamartino@gmail.com>
2013-03-11 17:51:14 +0100

    added power level

--- test.c
+++ test.c
@@ -21,16 +21,17 @@ void exit_signal(int id){
 }
 
 
-void bssid_found(const u_char *bssid){
+void bssid_found(const u_char *bssid, int8_t power){
     if(strncmp(bssid, broadcastaddr, 6) != 0)
-    printf("%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx\n",
-            bssid[0],bssid[1],bssid[2],bssid[3],bssid[4],bssid[5]);
+    printf("%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx Power:%d\n",
+            bssid[0],bssid[1],bssid[2],bssid[3],bssid[4],bssid[5],power);
     
 }
 
 
 void process_packet(u_char* useless, const struct pcap_pkthdr* header, const u_char* packet ){
     unsigned char bssid[6];
+    int8_t power;
 
     //printf("Packet with length %d received con %hhx:%hhx\n", header->len,packet[18],packet[19]);
     /* First 18 bytes are radiotap header. We don't care about that*/ 
@@ -43,7 +44,9 @@ void process_packet(u_char* useless, const struct pcap_pkthdr* header, const u_c
        or of type 0x28 (QoS)
      */
     if (header->len > 18){
+        power = packet[14];
         
+
         switch (packet[18]){
         
         case 0x40:
@@ -52,16 +55,16 @@ void process_packet(u_char* useless, const struct pcap_pkthdr* header, const u_c
         case 0x48:
         case 0x88:
         if (header->len >= 40){
-            bssid_found(packet+22);
-            bssid_found(packet+28);
-            bssid_found(packet+34);
+            bssid_found(packet+22, power);
+            bssid_found(packet+28, power);
+            bssid_found(packet+34, power);
         }
         break;
 
         case 0xd4:
         case 0xc4:
         if (header->len == 28){
-            bssid_found(packet+22);
+            bssid_found(packet+22, power);
         }
         break;
 
@@ -73,14 +76,14 @@ void process_packet(u_char* useless, const struct pcap_pkthdr* header, const u_c
     
 }
 
-int main(){
+int main(int argc, char **argv){
     char error_buffer[100];
     int status = 0;
 
     signal(SIGINT, exit_signal); /* Ctrl-C */
     signal(SIGQUIT, exit_signal); /* Ctrl-\ */
 
-    pcap_handle = pcap_create("wlan0", error_buffer);
+    pcap_handle = pcap_create(argv[argc-1], error_buffer);
     if (pcap_handle == NULL){
         printf("Error\n");
     }
